ac9cd81e1f610c5f339795beadcaaf9a3f57118e
Aovthz <47479122+Mandaranian-Cactus@users.noreply.github.com>
2020-04-15 20:46:10 -0400

    Add files via upload

--- Procedural_Tree_Generation/Poisson Disk & Perlin Noise (Better Neighbor Check).py
+++ Procedural_Tree_Generation/Poisson Disk & Perlin Noise (Better Neighbor Check).py
@@ -5,7 +5,7 @@ import random
 from collections import deque
 from Procedural_Tree_Generation import perlin_noise
 from datetime import datetime
-
+import time
 
 class Window:
     def __init__(self, w, h):
@@ -40,11 +40,17 @@ def generate_random_point_around(x, y, min_dist):
     return new_x, new_y
 
 
-def in_neighborhood(x, y, min_dist, li):
-    for nx, ny in li:
-        dist = math.sqrt((nx - x) ** 2 + (ny - y) ** 2)
-        if dist < min_dist:
-            return True  # at least 1 too close neighbor
+def in_neighborhood(x, y, min_dist, grid, cell_size):
+
+    grid_x, grid_y = image_to_grid(x, y, cell_size)
+    for ny in range(grid_y - 2, grid_y + 2 + 1):
+        for nx in range(grid_x - 2, grid_x + 2 + 1):
+            if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid):  # Border check (Using grid index format)
+                if len(grid[ny][nx]) > 0:  # Check to make sure there is/are neighbor(s) here
+                    for coord_nx, coord_ny in grid[ny][nx]:
+                        dist = math.sqrt((x - coord_nx) ** 2 + (y - coord_ny) ** 2)  # Calculate distance to neighbor
+                        if dist < min_dist:
+                            return True  # at least 1 too close neighbor
 
     return False  # no too close neighbor(s)
 
@@ -57,23 +63,36 @@ def convert_range(high, low, num):
 
 
 def generate_poisson(point_gen_cnt, min_radius, max_radius, perlin_noise):
+    # Note that "perlin noise" is technically the screen itself
 
+    cell_size = max_radius
     process_li = []
     sample_points = deque()
+    # The relationship between "grid" and "perlin_noise" is that "perlin_noise" is a coordinate system
+    # while "grid" is a grid used to split up "perlin_noise" into chunks
+    grid_w = int(len(perlin_noise[0])/cell_size + 0.9999999)  # Convert into grid index format
+    grid_h = int(len(perlin_noise)/cell_size + 0.9999999)  # Convert into grid index format
+    grid = [[[] for i in range(grid_w)] for i in range(grid_h)]  # It should be noted that we can store several points and not just one in a single chunk
 
+    # Generate random point onscreen
     x, y = random.randrange(len(perlin_noise[0])), random.randrange(len(perlin_noise))
     sample_points.append((x, y))
     process_li.append((x, y))
+    grid_x, grid_y = image_to_grid(x, y, cell_size)
+    grid[grid_y][grid_x].append((x, y))
 
     while process_li:
-        x, y = process_li.pop(random.randint(0, len(process_li) - 1))
+        x, y = process_li.pop()
         min_dist = min_radius + convert_range(0.8 ** 2, 0.2 ** 2, (1 - perlin_noise[int(y)][int(x)]) ** 2) * (max_radius - min_radius)  # The raise to power of 2 is used to create more polarity
         for i in range(point_gen_cnt):
             new_x, new_y = generate_random_point_around(x, y, min_dist)
             if 0 <= new_x <= len(perlin_noise[0]) and 0 <= new_y <= len(perlin_noise):  # Check to see if its within the borders
-                if in_neighborhood(new_x, new_y, min_dist, sample_points) == False:  # Check to see if no neighbors are too close
+                if in_neighborhood(new_x, new_y, min_dist, grid, cell_size) == False:  # Check to see if no neighbors are too close
                     process_li.append((new_x, new_y))
                     sample_points.append((new_x, new_y))
+                    # Convert the new coordinate into grid format and add it into the grid
+                    grid_x, grid_y = image_to_grid(new_x, new_y, cell_size)
+                    grid[grid_y][grid_x].append((new_x, new_y))
 
     return sample_points
 
@@ -89,8 +108,7 @@ for x, y in points:
     pygame.draw.rect(screen.screen, (255, 255, 255), (int(x), int(y), 2, 2))
 pygame.display.update()
 
-print(datetime.now() - startTime)
-
+print(datetime.now() - startTime)  # Print Runtime
 
 while True:
 
